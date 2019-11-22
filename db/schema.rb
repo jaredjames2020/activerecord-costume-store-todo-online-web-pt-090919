fbea8584636576c47da462d2863b11371e652bc7
Jared S <jaredjames2020@gmail.com>
2019-11-22 18:21:53 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.datetime "closing_time"
   end
 
-  create_table "haunted_houses", force: :cascade do |t|
+  create_table "costume_stores", force: :cascade do |t|
     t.string "name"
     t.string "location"
     t.string "theme"
