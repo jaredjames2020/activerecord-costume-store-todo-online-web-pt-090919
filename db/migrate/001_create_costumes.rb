c2239dca9b11c9b3b2377775cc1bd01190e7c89b
Jared S <jaredjames2020@gmail.com>
2019-11-22 19:23:40 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,7 +11,7 @@
 
 class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
-    create_table :costumes do |t|
+    create_table :costume do |t|
       t.string :name
       t.integer :price
       t.string :image_url
